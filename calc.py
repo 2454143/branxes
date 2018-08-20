967bd427119f502bcebae8e742359ea48ae26207
edelber.chinaglia <edelber@usp.br>
2018-08-20 16:55:11 -0300

    testes

--- calc.py
+++ calc.py
@@ -2,4 +2,8 @@ num1=1
 num2=2
 print("o soma de dois números é")
 print(num1+num2)
+print("a multiplicação de dois números é")
+print(num1*num2)
+print("a subtraçao de dois números é")
+print(num1-num2)
 
