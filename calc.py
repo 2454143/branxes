a366eb6b0288d25d78772171f5fad979cccdb42e
edelber.chinaglia <edelber@usp.br>
2018-08-20 17:01:02 -0300

    teste:

--- calc.py
+++ calc.py
@@ -6,4 +6,7 @@ print("a multiplicação de dois números é")
 print(num1*num2)
 print("a subtraçao de dois números é")
 print(num1-num2)
+print("a divisao de dois números é")
+print(num2/num1)
+print(acabou)
 
